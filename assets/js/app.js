4a26507dcdd9c146848a79d4cee78fe58f0e79e0
Rémi Mayeux <remimayeux@gmail.com>
2019-12-08 13:28:03 +0100

    search bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,17 @@ buttonAdvisors.addEventListener('click', () => {
     }
 });
 
+const myInput = document.getElementById('myInput');
+myInput.addEventListener('keyup', () => {
+    const filter = myInput.value.toUpperCase();
+    const cardAdvisors = document.getElementsByClassName('cardAdvisors');
+    for (let i = 0; i < cardAdvisors.length; i += 1) {
+        const h4 = cardAdvisors[i].getElementsByTagName('h4')[0];
+        const txtValue = h4.textContent || h4.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            cardAdvisors[i].style.display = '';
+        } else {
+            cardAdvisors[i].style.display = 'none';
+        }
+    }
+});
