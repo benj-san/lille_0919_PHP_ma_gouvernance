31cf9c74cd64383fbb82c0652b5758394675fa05
Nathan Vanpeene <nathv59@gmail.com>
2019-12-09 11:36:15 +0100

    correction css

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,18 +1,1 @@
 require('../scss/board.scss');
-
-const drag1 = document.getElementById('cardAdvisor');
-
-let allowDrop = (ev) => {
-    ev.preventDefault();
-};
-
-let drag = (ev) => {
-    ev.dataTransfer.setData('text', ev.target.id);
-};
-
-let drop = (ev) => {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData('text');
-    ev.target.appendChild(document.getElementById(data));
-    drag1.classList.toggle('color');
-};
