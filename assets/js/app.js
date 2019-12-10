65e25f7febcc60b8d8e8683f4e95150da5674ddd
Rémi Mayeux <remimayeux@gmail.com>
2019-12-10 11:19:13 +0100

    ajout modal description demande

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,12 @@ const boards = document.getElementById('Boards');
 const buttonDemand = document.getElementById('button-newdemand');
 const demandeFormulaire = document.getElementById('demandeFormulaire');
 const filterContainer = document.getElementById('filterContainer');
+const selectFormulaireDemande = document.getElementsByClassName('selectFormulaireDemande');
+const tagsContainer = document.getElementById('tags');
+const cardsDemande = document.getElementsByClassName('card-horizontal');
+const modalDescription = document.getElementsByClassName('modalDescription');
+const cardClient = document.getElementsByClassName('cardClient');
+const tags = document.getElementsByClassName('tags');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
@@ -39,4 +45,38 @@ buttonDemand.addEventListener('click', () => {
     demandeFormulaire.classList.toggle('hidden');
     filterContainer.classList.toggle('hidden');
     buttonDemand.classList.toggle('hidden');
+});
+
+selectFormulaireDemande.forEach(item => item.addEventListener('change', () => {
+    const div = document.createElement('div');
+    div.innerHTML = item.value;
+    div.classList.add('tags');
+    div.addEventListener('click', () => {
+        tagsContainer.removeChild(div);
+    });
+    tagsContainer.appendChild(div);
+}));
+
+cardsDemande.forEach(item => item.addEventListener('click', () => {
+    filterContainer.classList.toggle('hidden');
+}));
+
+for (let i = 0; i < cardsDemande.length; i++) {
+    cardsDemande[i].addEventListener('click', () => {
+        modalDescription[i].classList.remove('hidden');
+        buttonDemand.classList.add('hidden');
+        cardClient.forEach(item => item.classList.add('hidden'));
+    });
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target.id === 'filterContainer') {
+        for (let i = 0; i < cardsDemande.length; i++) {
+            modalDescription[i].classList.add('hidden');
+            filterContainer.classList.add('hidden');
+            buttonDemand.classList.remove('hidden');
+            cardClient.forEach(item => item.classList.remove('hidden'));
+        }
+        demandeFormulaire.classList.add('hidden');
+    }
 });
