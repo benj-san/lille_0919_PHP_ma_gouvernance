b30d95ca290f1985aece90705477ed53210a3c26
Rémi Mayeux <remimayeux@gmail.com>
2019-12-11 10:25:37 +0100

    Deleting h4 and h3 then added navbar

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,7 +13,6 @@ class AdminController extends AbstractController
     public function index()
     {
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
         ]);
     }
 }
