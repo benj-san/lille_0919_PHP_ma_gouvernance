b3bb14d55d13fcebace4ba130470563810e2907f
Nathan Vanpeene <nathv59@gmail.com>
2019-12-12 09:36:47 +0100

    modif css

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,6 +1,7 @@
 require('../scss/board.scss');
 
 const cardAdvisorJS = document.querySelectorAll('div.cardAdvisorJS');
+const addCardAdvisor = document.querySelectorAll('div.addCardAdvisor');
 const boardEdit = document.getElementById('boardEdit');
 const boardEditImg = document.getElementById('boardEditImg');
 const boardEditH2 = document.getElementById('boardEditH2');
@@ -10,28 +11,28 @@ const buttonValidateSynopsis = document.getElementById('buttonValidateSynopsis')
 const deleteAdvisor = document.querySelectorAll('div.deleteAdvisor');
 const definitiveDeleteAdvisor = document.querySelectorAll('button.definitiveDeleteAdvisor');
 const boardAdvisors = document.getElementById('boardAdvisors');
+const cardAdvisorAndAdd = document.querySelectorAll('div.cardAdvisorAndAdd');
 
 for (let i = 0; i < cardAdvisorJS.length; i++) {
-    cardAdvisorJS[i].addEventListener('click', () => {
+    addCardAdvisor[i].addEventListener('click', () => {
         behind.classList.add('display');
         commentAdvisor.classList.add('display');
 
         buttonValidateSynopsis.addEventListener('click', () => {
-            boardEdit.appendChild(cardAdvisorJS[i]);
+            boardEdit.appendChild(cardAdvisorAndAdd[i]);
             boardEditImg.classList.add('none');
             boardEditH2.classList.add('none');
             behind.classList.remove('display');
             commentAdvisor.classList.remove('display');
 
-            if (cardAdvisorJS[i].parentNode.id === 'boardEdit') {
-                cardAdvisorJS[i].removeEventListener('click', () => {});
+            if (cardAdvisorAndAdd[i].parentNode.id === 'boardEdit') {
                 boardEdit.classList.add('displayAdvisor');
                 cardAdvisorJS[i].addEventListener('click', () => {
                     behind.classList.add('display');
                     commentAdvisor.classList.remove('display');
                     deleteAdvisor[i].classList.add('display');
                     definitiveDeleteAdvisor[i].addEventListener('click', () => {
-                        boardAdvisors.appendChild(cardAdvisorJS[i]);
+                        boardAdvisors.appendChild(cardAdvisorAndAdd[i]);
                         behind.classList.remove('display');
                         deleteAdvisor[i].classList.remove('display');
                     });
