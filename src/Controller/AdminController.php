921ad5e30c74ed6a3b05a6d1424b13ea3fa86791
Rémi Mayeux <remimayeux@gmail.com>
2019-12-13 10:17:55 +0100

    Adding demands is functional

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Demand;
+use App\Form\DemandType;
 use App\Repository\AdvisorRepository;
 use App\Repository\DemandRepository;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
@@ -16,22 +19,34 @@ class AdminController extends AbstractController
      * @param AdvisorRepository $advisorRepository
      * @param DemandRepository $demandRepository
      * @param TagRepository $tagRepository
+     * @param Request $request
      * @return Response
      */
     public function index(
         AdvisorRepository $advisorRepository,
         DemandRepository $demandRepository,
-        TagRepository $tagRepository
+        TagRepository $tagRepository,
+        Request $request
     ) {
         $demands = $demandRepository ->findAll();
         $advisors = $advisorRepository->findAll();
         $tags = $tagRepository->findAll();
+
+        $demand = new Demand();
+        $form = $this->createForm(DemandType::class, $demand);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($demand);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin');
+        }
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
             'advisors' => $advisors,
             'demands'=>$demands,
             'tags' => $tags,
-
+            'formDemand' =>$form->createView()
         ]);
     }
 }
---
+++ src/Form/DemandType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Demand;
+use App\Entity\Tag;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class DemandType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('client')
+            ->add(
+                'clientRequest',
+                TextareaType::class,
+                ['label' => 'Besoins']
+            )
+            ->add('tags', EntityType::class, [
+                'attr' => ['class' => 'selectpicker',
+                    'data-live-search' => 'true'
+                    ],
+                'class' => Tag::class,
+                'choice_label' => 'category',
+                'multiple' => true,
+                'expanded' => false,
+                'by_reference' => false
+            ])
+            ->add('status');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Demand::class,
+        ]);
+    }
+}
