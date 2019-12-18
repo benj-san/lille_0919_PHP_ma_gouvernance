faedbab9134b843ef9cf1b9acc38005a628fb21b
Nathan Vanpeene <nathv59@gmail.com>
2019-12-18 11:11:52 +0100

    commencement à corriger les beugs js

--- assets/js/board.js
+++ assets/js/board.js
@@ -16,49 +16,38 @@ const cardAdvisorAndAdd = document.querySelectorAll('div.cardAdvisorAndAdd');
 const idAdvisor = document.querySelectorAll('p.idAdvisor');
 const checkboxAdvisor = document.querySelectorAll('input');
 
-
-
-
-
 for (let i = 0; i < cardAdvisorJS.length; i++) {
-    definitiveDeleteAdvisor[i].addEventListener('click', () => {
-        for (let j = 0; j < checkboxAdvisor.length; j++) {
-            if (checkboxAdvisor[j].type === 'checkbox') {
-                if (idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
-                    checkboxAdvisor[j].checked = false;
-                    boardAdvisors.appendChild(cardAdvisorAndAdd[i]);
-                    behind.classList.remove('display');
-                    deleteAdvisor[i].classList.remove('display');
-                }
-            }
-        }
-    });
     for (let j = 0; j < checkboxAdvisor.length; j++) {
         if (checkboxAdvisor[j].type === 'checkbox' && idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
             if (checkboxAdvisor[j].checked) {
                 boardEdit.appendChild(cardAdvisorAndAdd[i]);
+                boardEdit.classList.add('displayAdvisor');
+                cardAdvisorJS[i].classList.add('display');
                 boardEditImg.classList.add('none');
                 boardEditH2.classList.add('none');
-                boardEdit.classList.add('displayAdvisor');
-                cvAdvisor[i].classList.remove('display');
                 cardAdvisorJS[i].addEventListener('click', () => {
-                    behind.classList.add('display');
-                    commentAdvisor.classList.remove('display');
                     deleteAdvisor[i].classList.add('display');
+                });
+                behind.addEventListener('click', () => {
+                    deleteAdvisor[i].classList.remove('display');
+                });
+                definitiveDeleteAdvisor[i].addEventListener('click', () => {
+                    for (let j = 0; j < checkboxAdvisor.length; j++) {
+                        if (checkboxAdvisor[j].type === 'checkbox') {
+                            if (idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
+                                checkboxAdvisor[j].checked = false;
+                                boardAdvisors.appendChild(cardAdvisorAndAdd[i]);
+                                behind.classList.remove('display');
+                                deleteAdvisor[i].classList.remove('display');
+                            }
+                        }
+                    }
                 });
             }
         }
     }
     // To click to display the window to add an advisor
     addCardAdvisor[i].addEventListener('click', () => {
-        for (let j = 0; j < checkboxAdvisor.length; j++) {
-            if (checkboxAdvisor[j].type === 'checkbox') {
-                if (idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
-                    checkboxAdvisor[j].checked = true;
-                }
-            }
-        }
-
         behind.classList.add('display');
         commentAdvisor.classList.add('display');
         // To click to hide the different popups
@@ -77,6 +66,7 @@ for (let i = 0; i < cardAdvisorJS.length; i++) {
             for (let j = 0; j < checkboxAdvisor.length; j++) {
                 if (checkboxAdvisor[j].type === 'checkbox') {
                     if (idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
+                        checkboxAdvisor[j].checked = true;
                         boardEdit.appendChild(cardAdvisorAndAdd[i]);
                         boardEditImg.classList.add('none');
                         boardEditH2.classList.add('none');
@@ -109,8 +99,44 @@ for (let i = 0; i < cardAdvisorJS.length; i++) {
                     }
                 }
             }
+            buttonValidateSynopsis.removeEventListener('click', () => {
+                for (let j = 0; j < checkboxAdvisor.length; j++) {
+                    if (checkboxAdvisor[j].type === 'checkbox') {
+                        if (idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
+                            checkboxAdvisor[j].checked = true;
+                            boardEdit.appendChild(cardAdvisorAndAdd[i]);
+                            boardEditImg.classList.add('none');
+                            boardEditH2.classList.add('none');
+                            behind.classList.remove('display');
+                            commentAdvisor.classList.remove('display');
+                        }
+                    }
+                }
+                // Modify the css if an advisor goes into the board
+                if (cardAdvisorAndAdd[i].parentNode.id === 'boardEdit') {
+                    boardEdit.classList.add('displayAdvisor');
+                    // To click to display the window to remove an advisor
+                    cardAdvisorJS[i].addEventListener('click', () => {
+                        behind.classList.add('display');
+                        cvAdvisor[i].classList.remove('display');
+                        commentAdvisor.classList.remove('display');
+                        deleteAdvisor[i].classList.add('display');
+                    });
+                }
+            });
         });
     });
     // To click to display the advisors' CV
-
+    cardAdvisorJS[i].addEventListener('click', () => {
+        if (cardAdvisorAndAdd[i].parentNode.id === 'boardAdvisors') {
+            cvAdvisor[i].classList.add('display');
+            deleteAdvisor[i].remove('display');
+            behind.classList.add('display');
+            // To click to remove the advisors' CV
+            behind.addEventListener('click', () => {
+                cvAdvisor[i].classList.remove('display');
+                behind.classList.remove('display');
+            });
+        }
+    });
 }
