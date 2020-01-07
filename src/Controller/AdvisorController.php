3f42dd52eecdf997e284af72f6a3ec30fb7fcc84
Rémi Mayeux <remimayeux@gmail.com>
2020-01-07 16:13:52 +0100

    changed controller name

--- src/Controller/AdvisorController.php
+++ src/Controller/AdvisorController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FromAdvisorController extends AbstractController
+class AdvisorController extends AbstractController
 {
     /**
      * @Route("/formAdvisor", name="advisor")
--- src/Repository/AdvisorRepository.php
+++ src/Repository/AdvisorRepository.php
@@ -20,7 +20,7 @@ class AdvisorRepository extends ServiceEntityRepository
     }
 
     // /**
-    //  * @return Advisor[] Returns an array of Advisor objects
+    //  * @return AdvisorController[] Returns an array of AdvisorController objects
     //  */
     /*
     public function findByExampleField($value)
@@ -37,7 +37,7 @@ class AdvisorRepository extends ServiceEntityRepository
     */
 
     /*
-    public function findOneBySomeField($value): ?Advisor
+    public function findOneBySomeField($value): ?AdvisorController
     {
         return $this->createQueryBuilder('a')
             ->andWhere('a.exampleField = :val')
