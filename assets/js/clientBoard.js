76bffa441579ea7d958228a9ee8cf64954845f41
clemcx <clemencecaux@gmail.com>
2020-01-14 17:52:26 +0100

    linkedin button +css

--- assets/js/clientBoard.js
+++ assets/js/clientBoard.js
@@ -1,20 +1,18 @@
 require('../scss/clientBoard.scss');
 
 const cvAdvisor = document.querySelectorAll('div.cvAdvisor');
-const cardAdvisor = document.querySelectorAll('div.cardAdvisors');
+const advisorInfo = document.querySelectorAll('div.advisorInfo');
 const behind = document.getElementById('behind');
 
 
-for (let i = 0; i < cardAdvisor.length; i += 1) {
-    cardAdvisor[i].addEventListener('click', () => {
-        if (cardAdvisor[i].parentNode.id === 'boardContent') {
-            cvAdvisor[i].classList.add('display');
-            behind.classList.add('display');
-            // To click to remove the a.classList.add('displayclientAdvisor');dvisors' CV
-            behind.addEventListener('click', () => {
-                cvAdvisor[i].classList.remove('display');
-                behind.classList.remove('display');
-            });
-        }
+for (let i = 0; i < advisorInfo.length; i += 1) {
+    advisorInfo[i].addEventListener('click', () => {
+        cvAdvisor[i].classList.add('display');
+        behind.classList.add('display');
+        // To click to remove the a.classList.add('displayclientAdvisor');dvisors' CV
+        behind.addEventListener('click', () => {
+            cvAdvisor[i].classList.remove('display');
+            behind.classList.remove('display');
+        });
     });
 }
