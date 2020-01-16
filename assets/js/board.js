e217de70ddb8a0e3ab8256ef9bfd2d334602f4f5
Nathan Vanpeene <nathv59@gmail.com>
2020-01-16 11:55:16 +0100

    modif css

--- assets/js/board.js
+++ assets/js/board.js
@@ -8,26 +8,27 @@ const commentAdvisor = document.querySelectorAll('div.commentAdvisor');
 const deleteAdvisor = document.querySelectorAll('div.deleteAdvisor');
 const definitiveDeleteAdvisor = document.querySelectorAll('button.definitiveDeleteAdvisor');
 const boardAdvisors = document.getElementById('boardAdvisors');
-const cardAdvisorAndAdd = document.querySelectorAll('div.cardAdvisorAndAdd');
 const idAdvisor = document.querySelectorAll('p.idAdvisor');
 const checkboxAdvisor = document.querySelectorAll('input');
 const iconAdd = document.querySelectorAll('img.iconAdd');
 const iconCv = document.querySelectorAll('img.iconCv');
 const iconDelete = document.querySelectorAll('img.iconDelete');
+const addAdvisors = document.getElementById('addAdvisors');
+const viewBoardAndIcon = document.getElementById('viewBoardAndIcon');
 
 for (let i = 0; i < cardAdvisorJS.length; i += 1) {
     // Advisors in DB
     for (let j = 0; j < checkboxAdvisor.length; j += 1) {
         if (checkboxAdvisor[j].type === 'checkbox' && idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
             if (checkboxAdvisor[j].checked) {
-                boardEdit.appendChild(cardAdvisorAndAdd[i]);
+                boardEdit.appendChild(cardAdvisorJS[i]);
                 boardEdit.classList.add('displayAdvisor');
             }
         }
     }
     // Advisors not in DB
     // Modify the css if an advisor goes into the board
-    if (cardAdvisorAndAdd[i].parentNode.id === 'boardEdit') {
+    if (cardAdvisorJS[i].parentNode.id === 'boardEdit') {
         boardEdit.classList.add('displayAdvisor');
     }
     // Displays the different windows according to the icons
@@ -56,7 +57,7 @@ for (let i = 0; i < cardAdvisorJS.length; i += 1) {
             if (checkboxAdvisor[j].type === 'checkbox') {
                 if (idAdvisor[i].innerHTML === checkboxAdvisor[j].value) {
                     checkboxAdvisor[j].checked = false;
-                    boardAdvisors.appendChild(cardAdvisorAndAdd[i]);
+                    boardAdvisors.appendChild(cardAdvisorJS[i]);
                     behind.classList.remove('display');
                     deleteAdvisor[i].classList.remove('display');
                 }
@@ -64,3 +65,9 @@ for (let i = 0; i < cardAdvisorJS.length; i += 1) {
         }
     });
 }
+
+addAdvisors.addEventListener('click', () => {
+    boardAdvisors.classList.add('display');
+    boardEdit.classList.add('hidden');
+    viewBoardAndIcon.classList.add('hidden');
+});
