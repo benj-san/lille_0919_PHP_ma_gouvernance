47058f6d871e940b7c0528d9784bed4671d7cedc
Orianne0605 <otanguy28@gmail.com>
2020-01-19 23:42:36 +0100

    work no finished

--- src/Controller/AdvisorController.php
+++ src/Controller/AdvisorController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// import client class
+use LinkedIn\Client;
 
 class AdvisorController extends AbstractController
 {
@@ -16,5 +18,13 @@ class AdvisorController extends AbstractController
     {
         return $this->render('advisor/advisor.html.twig', [
         ]);
+
+        // instantiate the Linkedin client
+        $client = new Client(
+            '86z6naqd76gryg',
+            'rTQu4n1L70glQ78d'
+        );
+        $redirectUrl = $client->getRedirectUrl();
+        $client->setRedirectUrl('http://magouvernance.fr/');
     }
 }
