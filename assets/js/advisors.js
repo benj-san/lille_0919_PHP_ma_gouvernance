24812a05b02429b3d916a6ea871a822f14cf863f
clemcx <clemencecaux@gmail.com>
2020-01-22 13:00:25 +0100

    indentation fix

--- assets/js/advisors.js
+++ assets/js/advisors.js
@@ -7,15 +7,15 @@ const myInput = document.getElementById('myInput');
 const filterContainer = document.getElementById('filterContainer');
 
 for (let i = 0; i < cardAdvisorJS.length; i += 1) {
-    //cardAdvisorJS[i].addEventListener('click', () => {
-      //  cvAdvisor[i].classList.add('display');
-       // filterContainer.classList.remove('hidden');
-        // To click to remove the advisors' CV
-      //  filterContainer.addEventListener('click', () => {
-        //    cvAdvisor[i].classList.remove('display');
-          //  filterContainer.classList.add('hidden');
-        //});
-  //  });
+    // cardAdvisorJS[i].addEventListener('click', () => {
+    //  cvAdvisor[i].classList.add('display');
+    // filterContainer.classList.remove('hidden');
+    // To click to remove the advisors' CV
+    //  filterContainer.addEventListener('click', () => {
+    //    cvAdvisor[i].classList.remove('display');
+    //  filterContainer.classList.add('hidden');
+    // });
+    //  });
 
     iconCv[i].addEventListener('click', () => {
         cvAdvisor[i].classList.add('display');
@@ -39,6 +39,4 @@ myInput.addEventListener('keyup', () => {
             cardAdvisors[i].style.display = 'none';
         }
     }
-
-
 });
