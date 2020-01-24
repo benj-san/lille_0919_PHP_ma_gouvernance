87dab3123a8139f1282c9d7b48bcc4eb503e01a0
Rémi Mayeux <remimayeux@gmail.com>
2020-01-24 14:13:45 +0100

    fixed indentation

--- src/Controller/AdvisorController.php
+++ src/Controller/AdvisorController.php
@@ -14,7 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
