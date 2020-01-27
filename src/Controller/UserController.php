47e9bd090406774b70f4bb4632df8c2aff72e832
Najwa <najat.ciesielczyk@sfr.fr>
2020-01-27 17:35:40 +0100

    c pr : si on est connecté on se met sur la route demands en mettant loachost:8000/

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,9 +17,9 @@ class UserController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+             return $this->redirectToRoute('demands');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
